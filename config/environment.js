18fb31bf4307eba943beafbc09ec214242ca6bd1
sumit kumar <62461050+sumitjangir123@users.noreply.github.com>
2020-10-21 19:27:16 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -23,15 +23,15 @@ const development= {
         secure: false, // true for 465, false for other ports
         auth: {
           user: 'thethunderbirdus@gmail.com',
-          pass: 'thunderbird1234'
+          pass: ''
         }
     },
-    google_client_id: "922852696533-qrr4lrcdb3uj5sjdjcqi1g90fc5mpsoq.apps.googleusercontent.com",
-    google_client_secret: "Vu8_IYaYygO-z07kp80i7356",
+    google_client_id: "",
+    google_client_secret: "",
     google_call_back_url: "http://localhost:8000/users/auth/google/callback",
 
-    facebook_client_id: "549980429009358",
-    facebook_client_secret: "3f007a155b585a2b535979052b07b5fd",
+    facebook_client_id: "",
+    facebook_client_secret: "",
     facebook_call_back_url: "http://localhost:8000/users/auth/facebook/callback",
 
     jwt_secret: 'codeial',
